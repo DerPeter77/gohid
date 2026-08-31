8fc522eb60fc237dd4371c53c0a0f271fc2a1805
DerPeter77 <145033939+DerPeter77@users.noreply.github.com>
2026-08-31 18:23:39 +0200

    removed example from main

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 
-	logitechprox "github.com/DerPeter77/gohid/devices/logitechProX"
 	"github.com/DerPeter77/gohid/internal/linux"
 )
 
@@ -25,31 +24,6 @@ func main() {
 				fmt.Printf("Gerät: \"%v\" Pfad: \"%v\"\n", device.Name, device.Path)
 			}
 			return
-		}
-	}
-
-	devPath := "/dev/hidraw5"
-	readChan := make(chan []byte, 1)
-
-	// 1. Reader im Hintergrund starten (weil Read ewig blockiert)
-	go linux.ReadHidFile(devPath, readChan)
-
-	// 3. Batterie-Abfrage direkt und einfach abfeuern!
-	request := []byte{
-		0x11, 0xFF, 0x06, 0x09, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x00,
-	}
-
-	fmt.Println("Sende Batterie-Anfrage...")
-	if err := linux.WriteHidFile(devPath, request); err != nil {
-		log.Fatalf("Fehler beim Senden: %v", err)
-	}
-
-	for data := range readChan {
-		if mv, percent, ok := logitechprox.ParseBattery(data); ok {
-			fmt.Printf("Akkustand empfangen: %d mV (%d%%)\n", mv, percent)
-			close(readChan)
 		}
 	}
 }
