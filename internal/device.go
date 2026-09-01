fb32698eebb25461f276da25dd5d7f42baf7c91d
DerPeter77 <145033939+DerPeter77@users.noreply.github.com>
2026-09-01 18:40:45 +0200

    WIP Refactor

--- internal/device.go
+++ internal/device.go
@@ -1,15 +1,31 @@
-package linux
+package internal
 
 import (
 	"fmt"
 	"os"
 )
 
+type Device struct {
+	path string
+}
+
+// NewDevice creates and returns a new Device with the path given as Argument
+func NewDevice(path string) Device {
+	return Device{
+		path: path,
+	}
+}
+
+// GetDevicePath returns the Device Path
+func (device Device) GetDevicePath() string {
+	return device.path
+}
+
 // ReadHidFile reads the HID File from hidpath and sends all of the data to the given channel.
 // Max Buffer for reading the file is at [64]byte.
 // Returns error or nil if successfull.
-func ReadHidFile(hidpath string) (<-chan []byte, error) {
-	file, err := os.OpenFile(hidpath, os.O_RDWR, 0666)
+func (device Device) ReadHidFile() (<-chan []byte, error) {
+	file, err := os.OpenFile(device.path, os.O_RDWR, 0666)
 	if err != nil {
 		return nil, err
 	}
@@ -36,33 +52,10 @@ func ReadHidFile(hidpath string) (<-chan []byte, error) {
 	return ch, nil
 }
 
-// Same like ReadHidFile but takes the channel given in Arguments. Use this function only if necessary
-func ReadHidFileChannel(hidpath string, ch chan<- []byte) error {
-	file, err := os.OpenFile(hidpath, os.O_RDWR, 0666)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	for {
-		buf := make([]byte, 64)
-		n, err := file.Read(buf)
-		if err != nil {
-			close(ch)
-			return err
-		}
-
-		data := make([]byte, n)
-		copy(data, buf[:n])
-
-		ch <- data
-	}
-}
-
 // WriteHidFile writes data to the file at hidpath.
 // Returns error or nil if successfull.
-func WriteHidFile(hidpath string, data []byte) error {
-	file, err := os.OpenFile(hidpath, os.O_WRONLY, 0666)
+func (device Device) WriteHidFile(data []byte) error {
+	file, err := os.OpenFile(device.path, os.O_WRONLY, 0666)
 	if err != nil {
 		return err
 	}
