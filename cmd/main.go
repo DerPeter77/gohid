efe656c80d0b7e10171819640e3bb619649cfa07
DerPeter77 <145033939+DerPeter77@users.noreply.github.com>
2026-09-05 18:46:23 +0200

    added cmd main

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/DerPeter77/gohid"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		args := os.Args[1]
+
+		switch args {
+		case "list":
+			devices_list, err := gohid.GetAllUsbDevices()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Print("USB Devicelist:\n\n")
+			for _, device := range devices_list {
+				fmt.Printf("Gerät: \"%v\" Pfad: \"%v\"\n", device.Name, device.Path)
+			}
+			return
+		}
+	}
+}
